2df428197c6e4eb00ea4293e29fc50ffec7a950c
Blaez-Dev <kenzatu.konata.45@gmail.com>
2018-10-31 02:35:10 -0400

    Update EntityWinterBeowolf.java

--- src/main/java/be/bluexin/rwbym/entity/EntityWinterBeowolf.java
+++ src/main/java/be/bluexin/rwbym/entity/EntityWinterBeowolf.java
@@ -64,11 +64,7 @@ public class EntityWinterBeowolf extends EntityMob {
         super.onLivingUpdate();
         counter += 1;
 
-<<<<<<< HEAD
         if(counter >= 1200){
-=======
-        if(counter >= 6000){
->>>>>>> d07a651f5a1b50c14f9c32a18511f48bbca908be
             this.setDead();
         }
     }
