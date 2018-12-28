a1a7102c0e3f837c20fa4427ca330c5f6a9be24c
Pi Man <>
2018-12-27 17:46:41 -0700

    adds base for Weiss semblance

---
+++ src/main/java/be/bluexin/rwbym/capabilities/Weiss/WeissProvider.java
@@ -0,0 +1,37 @@
+package be.bluexin.rwbym.capabilities.Weiss;
+
+import be.bluexin.rwbym.capabilities.ruby.IRuby;
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityInject;
+import net.minecraftforge.common.capabilities.ICapabilitySerializable;
+
+public class WeissProvider implements ICapabilitySerializable<NBTBase> {
+	
+	@CapabilityInject (IRuby.class)
+	public static final Capability<IWeiss> WEISS_CAP = null;
+	
+	private IWeiss instance = WEISS_CAP.getDefaultInstance();
+
+	@Override
+	public boolean hasCapability(Capability<?> capability, EnumFacing facing) {
+		return capability == WEISS_CAP;
+	}
+
+	@Override
+	public <T> T getCapability(Capability<T> capability, EnumFacing facing) {
+		return hasCapability(capability, facing) ? WEISS_CAP.<T> cast(this.instance) : null;
+	}
+
+	@Override
+	public NBTBase serializeNBT() {
+		return WEISS_CAP.getStorage().writeNBT(WEISS_CAP, this.instance, null);
+	}
+
+	@Override
+	public void deserializeNBT(NBTBase nbt) {
+		WEISS_CAP.getStorage().readNBT(WEISS_CAP, this.instance, null, nbt);
+	}
+
+}
