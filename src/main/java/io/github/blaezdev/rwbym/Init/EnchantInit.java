f6fd68f6c409b3252ba51b5d488b2daa7ce8b0c8
Blaez-Dev <kenzatu.konata.45@gmail.com>
2021-03-17 22:23:17 -0400

    Weapon Modifiers added to shop inventory.. auto updates and expandable.

--- src/main/java/io/github/blaezdev/rwbym/Init/EnchantInit.java
+++ src/main/java/io/github/blaezdev/rwbym/Init/EnchantInit.java
@@ -16,6 +16,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Mod.EventBusSubscriber(modid=RWBYModels.MODID)
 public class EnchantInit {
@@ -24,6 +25,31 @@ public class EnchantInit {
     public static final Enchantment KNOCK_SHOT = new EnchantmentKnockShot();  //Shot Based Disallow Multiple Shot Modifiers
     public static final Enchantment AURA_SIPHON = new EnchantmentAuraSiphon();  //Kill Based Disallow Multiple Kill Modifiers
     public static final Enchantment SCAVENGER = new EnchantmentScavenger(); //Kill Based Disallow Multiple Kill Modifiers
+
+
+    public static ArrayList<Enchantment> shotmodifier = new ArrayList<Enchantment>(){
+    };
+
+    public static ArrayList<Enchantment> killmodifier = new ArrayList<Enchantment>(){
+    };
+
+    public static Enchantment getShotmodifierEnchant(){
+        shotmodifier.add(EnchantInit.DOUBLE_SHOT);
+        shotmodifier.add(EnchantInit.KNOCK_SHOT);
+        Random rand = new Random();
+        int index = rand.nextInt(shotmodifier.size());
+        Enchantment randenchant = shotmodifier.get(index);
+        return randenchant;
+    }
+
+    public static Enchantment getKillmodifierEnchant(){
+        killmodifier.add(EnchantInit.AURA_SIPHON);
+        killmodifier.add(EnchantInit.SCAVENGER);
+        Random rand = new Random();
+        int index = rand.nextInt(killmodifier.size());
+        Enchantment randenchant = killmodifier.get(index);
+        return randenchant;
+    }
 
     /*
     @SubscribeEvent
